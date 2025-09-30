47d1e70c0a09e3eb9d72315c0155facd12d0f8a2
Félix Hernández Vieyra <71148989+Felix-Hz@users.noreply.github.com>
2025-09-30 20:44:33 +1300

    feat(repository): offset impl (#6)

---
+++ repository/offset.go
@@ -0,0 +1,45 @@
+package repository
+
+import (
+	. "remind0/db"
+
+	"gorm.io/gorm"
+)
+
+type offsetRepository struct {
+	dbClient *gorm.DB
+}
+
+type OffsetRepository interface {
+	// Get the existing offset or create a new one if it doesn't exist.
+	GetOrCreate() (*Offset, error)
+	// Update the offset value in the database to keep track of processed updates.
+	UpdateLastSeen(offset *Offset, id int) error
+}
+
+/**
+ * Factory method to initialise a repository.
+ */
+func OffsetRepositoryImpl(dbClient *gorm.DB) OffsetRepository {
+	return &offsetRepository{dbClient: dbClient}
+}
+
+func (r *offsetRepository) GetOrCreate() (*Offset, error) {
+	var offset Offset
+
+	result := r.dbClient.First(&offset)
+	if result.Error != nil {
+		offset = Offset{Offset: 0}
+
+		if err := r.dbClient.Create(&offset).Error; err != nil {
+			return nil, err
+		}
+	}
+
+	return &offset, nil
+}
+
+func (r *offsetRepository) UpdateLastSeen(offset *Offset, id int) error {
+	offset.Offset = id
+	return r.dbClient.Save(offset).Error
+}
